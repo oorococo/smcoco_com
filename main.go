1082a01cf73837fb6059d4f4c2d9b494af92c62a
oorococo <oorococo@gmail.com>
2017-01-04 13:32:27 +0800

    dfasdf

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/http2"
+)
+
 func main() {
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Add("Link", "<https://static.smcoco.com/auth/auth.css>; rel=preload;")
+
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	s := &http.Server{
+		Addr:           ":3003",
+		Handler:        nil,
+		ReadTimeout:    30 * time.Second,
+		WriteTimeout:   30 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	http2.ConfigureServer(s, nil)
+
+	log.Fatal(s.ListenAndServe())
 
 }
